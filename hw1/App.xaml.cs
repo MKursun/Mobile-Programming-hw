c182066846b7db778757530e886375dfa595da79
MKursun <melekkursun@anadolu.edu.tr>
2018-11-04 16:34:13 +0300

    Add files via upload

---
+++ hw1/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace hw1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var tabbeled = new TabbedPage();
+            tabbeled.Children.Add(new Hakkında());
+            tabbeled.Children.Add(new Kisiler());
+            tabbeled.Children.Add(new Altyapı());
+            tabbeled.Children.Add(new Dersler());
+
+
+            MainPage = tabbeled;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
